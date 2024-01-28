faecc08d94538dd5083aa98f6725ae908f71641f
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-01-28 07:04:29 +0400

    Update constants.py

--- infisical/constants.py
+++ infisical/constants.py
@@ -4,4 +4,4 @@ INFISICAL_URL = "https://app.infisical.com"
 AUTH_MODE_SERVICE_TOKEN = "service_token"
 AUTH_MODE_SERVICE_TOKEN_V3 = "service_token_v3"
 
-SERVICE_TOKEN_REGEX = re.compile(r"(st\.[a-f0-9]+\.[a-f0-9]+)\.([a-f0-9]+)")
+SERVICE_TOKEN_REGEX = re.compile(r"(st\.[a-f0-9-]+\.[a-f0-9]+)\.([a-f0-9]+)")
