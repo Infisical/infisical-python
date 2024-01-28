b1cf8fbb6cd6d9c7871f7b04736c396ccd84feb6
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-01-28 13:05:20 +0400

    Fixed python SDK for PG

--- infisical/api/models.py
+++ infisical/api/models.py
@@ -5,19 +5,19 @@ from pydantic import BaseModel, Field
 
 
 class GetServiceTokenDetailsUserResponse(BaseModel):
-    id: str = Field(..., alias="_id")
+    id: str = Field(..., alias="id")
     email: str
     created_at: datetime = Field(..., alias="createdAt")
     updated_at: datetime = Field(..., alias="updatedAt")
-    v: int = Field(..., alias="__v")
+    # v: int = Field(..., alias="__v")
     first_name: str = Field(..., alias="firstName")
     last_name: str = Field(..., alias="lastName")
 
 
 class GetServiceTokenDetailsResponse(BaseModel):
-    id: str = Field(..., alias="_id")
+    id: str = Field(..., alias="id")
     name: str
-    workspace: str
+    workspace: str = Field(..., alias="projectId")
     user: GetServiceTokenDetailsUserResponse
     expires_at: Optional[datetime] = Field(None, alias="expiresAt")
     encrypted_key: str = Field(..., alias="encryptedKey")
@@ -25,10 +25,10 @@ class GetServiceTokenDetailsResponse(BaseModel):
     tag: str
     created_at: datetime = Field(..., alias="createdAt")
     updated_at: datetime = Field(..., alias="updatedAt")
-    v: int = Field(..., alias="__v")
+    #v: int = Field(..., alias="__v")
 
 class KeyData(BaseModel):
-    id: str = Field(..., alias="_id")
+    id: str = Field(..., alias="id")
     workspace: str
     encrypted_key: str = Field(..., alias="encryptedKey")
     public_key: str = Field(..., alias="publicKey")
--- infisical/models/models.py
+++ infisical/models/models.py
@@ -6,7 +6,7 @@ from typing_extensions import Literal
 
 
 class Secret(BaseModel):
-    id: str = Field(..., alias="_id")
+    id: str = Field(..., alias="id")
     version: int
     workspace: str
     user: Optional[str]
@@ -36,7 +36,7 @@ class SecretBundle(BaseModel):
 
 
 class ServiceTokenData(BaseModel):
-    id: str = Field(..., alias="_id")
+    id: str = Field(..., alias="id")
     name: str
     workspace: str
     environment: str
